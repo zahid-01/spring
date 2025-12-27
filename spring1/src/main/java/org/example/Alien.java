ded4d81ae9225ea4572176dca35cdef548c23b27
zahid-01 <erzahidkhan101@gmail.com>
2025-12-27 18:09:50 +0530

    Property based injectoin

--- spring1/src/main/java/org/example/Alien.java
+++ spring1/src/main/java/org/example/Alien.java
@@ -1,7 +1,21 @@
 package org.example;
 
 public class Alien {
-    void code(){
+    private int age;
+
+    Alien() {
+        System.out.println("Alien constructor");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    void code() {
         System.out.println("Alien code");
     }
 }
