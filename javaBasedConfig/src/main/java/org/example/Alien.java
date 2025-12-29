30c311180042a48f9876a58c2e7ca6c9a1e3c0ee
zahid-01 <erzahidkhan101@gmail.com>
2025-12-29 11:15:14 +0530

    Scope and value annotation

--- javaBasedConfig/src/main/java/org/example/Alien.java
+++ javaBasedConfig/src/main/java/org/example/Alien.java
@@ -2,10 +2,14 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Alien {
+    @Value("21")
     private int age;
 
     @Autowired
@@ -33,6 +37,7 @@ public class Alien {
     }
 
     void code() {
+        System.out.println(age);
         comp.compile();
     }
 }
